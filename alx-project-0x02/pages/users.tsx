3facc1c4c32d4292ed4571dff00ed1f1d6bf067f
onebyte2016 <onebyte2016@gmail.com>
2025-07-23 08:56:27 +0000

    nextjs typscript and react proj

--- alx-project-0x02/pages/users.tsx
+++ alx-project-0x02/pages/users.tsx
@@ -1,21 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { UserProps } from '@/interfaces';
 import UserCard from '@/components/common/UserCard';
 import Header from '@/components/layout/Header';
 
-const UsersPage: React.FC = () => {
-  const [users, setUsers] = useState<UserProps[]>([]);
+interface UsersPageProps {
+  users: UserProps[];
+}
 
-  useEffect(() => {
-    const fetchUsers = async () => {
-      const res = await fetch('https://jsonplaceholder.typicode.com/users');
-      const data = await res.json();
-      setUsers(data);
-    };
-
-    fetchUsers();
-  }, []);
-
+const UsersPage: React.FC<UsersPageProps> = ({ users }) => {
   return (
     <>
       {/* <Header /> */}
@@ -35,4 +27,62 @@ const UsersPage: React.FC = () => {
   );
 };
 
+// Static generation function
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const users: UserProps[] = await res.json();
+
+  return {
+    props: {
+      users,
+    },
+  };
+};
+
 export default UsersPage;
+
+
+
+
+
+
+
+
+// import React, { useEffect, useState } from 'react';
+// import { UserProps } from '@/interfaces';
+// import UserCard from '@/components/common/UserCard';
+// import Header from '@/components/layout/Header';
+
+// const UsersPage: React.FC = () => {
+//   const [users, setUsers] = useState<UserProps[]>([]);
+
+//   useEffect(() => {
+//     const fetchUsers = async () => {
+//       const res = await fetch('https://jsonplaceholder.typicode.com/users');
+//       const data = await res.json();
+//       setUsers(data);
+//     };
+
+//     fetchUsers();
+//   }, []);
+
+//   return (
+//     <>
+//       {/* <Header /> */}
+//       <div className="p-6 space-y-4">
+//         <h1 className="text-2xl font-bold mb-4">Users</h1>
+//         {users.map((user) => (
+//           <UserCard
+//             key={user.id}
+//             id={user.id}
+//             name={user.name}
+//             email={user.email}
+//             address={user.address}
+//           />
+//         ))}
+//       </div>
+//     </>
+//   );
+// };
+
+// export default UsersPage;
